85d701895c1fc2a171b69d136d57641391dd98ba
Alumne Tarda <alumnet@localhost.localdomain>
2026-01-15 17:40:01 +0100

    first commit

---
+++ hibernate.java
@@ -0,0 +1,11 @@
+public class hibernate {
+
+   
+public static void main(String[] args) {
+    System.out.println("Hello, Hibernate!");
+    System.out.println("Welcome to the world of ORM!");
+    System.out.println("hola");
+
+}
+    
+}
