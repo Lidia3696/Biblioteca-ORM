78e97a4869e56db68be676cb2e7af348f85c3ead
Alumne Tarda <alumnet@localhost.localdomain>
2026-01-16 17:46:03 +0100

    persistence and new class usuario

--- hibernate.java
+++ hibernate.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
     System.out.println("Hello, Hibernate!");
     System.out.println("Welcome to the world of ORM!");
     System.out.println("hola");
-
+    
 }
     
 }
